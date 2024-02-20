502e5a4a81c670a2deb2c0b28d4d740581ed68ce
Ley Roy <atiwensolomon@gmail.com>
2024-02-20 15:57:07 +0000

    completed the setup

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
 	return (
 		<div className="">
-			<div className=" relative w-full flex flex-col items-center justify-center gap-3 py-10 text-white bg-logocolor mx-auto ">
+			<div className=" w-full flex flex-col items-center justify-center gap-3 py-10 text-white bg-logocolor mx-auto ">
 				<p className="text-3xl font-bold">
 					Find a career you will love
 				</p>
@@ -12,4 +12,7 @@ export default function Home() {
 					satisfaction, best salaries, and more
 				</h1>
 			</div>
-			<div c
+			<div className=" -mt-10 rounded-md mx-auto shadow-lg md:w-4/6 h-20 bg-white"></div>
+		</div>
+	);
+}
